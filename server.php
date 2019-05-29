8f79cc369eea269c790202b9748e4c0e494e3917
Damir15 <krosava_15d@mail.ru>
2019-05-29 23:34:45 +0600

    Added validation brackets

--- server.php
+++ server.php
@@ -2,6 +2,8 @@
 <?php
 error_reporting(E_ALL);
 
+require_once 'vendor/autoload.php';
+
 $connectionData = getopt('ip:port:', ['ip:', 'port:']);
 
 if (empty($connectionData) || !isset($connectionData['ip']) || !isset($connectionData['port'])) {
@@ -11,52 +13,55 @@ if (empty($connectionData) || !isset($connectionData['ip']) || !isset($connectio
 $ip   = $connectionData['ip'];
 $port = $connectionData['port'];
 
-error_reporting(E_ALL);
 set_time_limit(0);
-
-/* Turn on implicit output flushing so we see what we're getting
- * as it comes in. */
 ob_implicit_flush();
 
 if (($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false) {
-    echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n";
+    echo "Create failed, reason: " . socket_strerror(socket_last_error()) . "\n";
 }
 
 if (socket_bind($sock, $ip, $port) === false) {
-    echo "socket_bind() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
+    echo "Bind failed, reason: " . socket_strerror(socket_last_error($sock)) . "\n";
 }
 
-if (socket_listen($sock, 5) === false) {
-    echo "socket_listen() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
+if (socket_listen($sock) === false) {
+    echo "Listen failed, reason: " . socket_strerror(socket_last_error($sock)) . "\n";
 }
+
+$bracket = new Koind\Brackets();
 
 do {
     if (($msgsock = socket_accept($sock)) === false) {
-        echo "socket_accept() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
+        echo "Accept failed, reason: " . socket_strerror(socket_last_error($sock)) . "\n";
         break;
     }
-    /* Send instructions. */
-    $msg = "\nWelcome to the PHP Test Server. \n" .
-        "To quit, type 'quit'. To shut down the server type 'shutdown'.\n";
+
+    $msg = "\nWelcome to the brace checking server.\n To quit, type 'quit'. \n";
+
     socket_write($msgsock, $msg, strlen($msg));
 
     do {
         if (false === ($buf = socket_read($msgsock, 2048, PHP_NORMAL_READ))) {
-            echo "socket_read() failed: reason: " . socket_strerror(socket_last_error($msgsock)) . "\n";
+            echo "failed: reason: " . socket_strerror(socket_last_error($msgsock)) . "\n";
             break 2;
         }
+
         if (!$buf = trim($buf)) {
             continue;
         }
+
         if ($buf == 'quit') {
             break;
         }
-        if ($buf == 'shutdown') {
-            socket_close($msgsock);
-            break 2;
+
+        try {
+            $talkback = $bracket->checkString($buf) ? 'Brackets is correct' : 'Brackets is not correct';
+        } catch (\InvalidArgumentException $e) {
+            $talkback = $e->getMessage();
         }
-        $talkback = "PHP: You said '$buf'.\n";
+
         socket_write($msgsock, $talkback, strlen($talkback));
+
         echo "$buf\n";
     } while (true);
     socket_close($msgsock);
